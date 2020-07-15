e409584bf7489bcecebad4493be23e9210d1f734
神馬 健介 <jin35314031@gmail.com>
2020-07-15 10:56:28 +0900

    VueRouter HelloWorld

---
+++ hello/pages/index.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="container">
+    <p>{{message}}</p>
+    <hr>
+    <router-link to="/price">Price Page</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  data:function(){
+    return{
+      message:'Hello World!'
+    }
+  }
+}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family:
+    'Quicksand',
+    'Source Sans Pro',
+    -apple-system,
+    BlinkMacSystemFont,
+    'Segoe UI',
+    Roboto,
+    'Helvetica Neue',
+    Arial,
+    sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
---
+++ nuxt-first-exercise/pages/mypage.vue
@@ -0,0 +1,7 @@
+<template>
+    <section class="container">
+        <h2>My Page</h2>
+        <hr>
+        <router-link to = "/"> Top Page </router-link>
+    </section>
+</template>
