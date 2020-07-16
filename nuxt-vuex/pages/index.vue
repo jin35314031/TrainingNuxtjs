9c861689d7c3631f8f279d62335828236c0be579
神馬 健介 <jin35314031@gmail.com>
2020-07-16 16:34:27 +0900

    mutation

---
+++ nuxt-vuex/pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="container">
+    <p>{{ $store.state.message }}</p>
+    <button v-on:click="$store.commit('updateMessage')">Update</button>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
---
+++ nuxt-vuex/store/index.js
@@ -0,0 +1,18 @@
+import Vuex from 'vuex'
+
+const createStore = () => {
+    return new Vuex.Store({
+        state: function(){
+            return{
+                message:'Hello Vuex!'
+            }
+        },
+        mutations:{
+            updateMessage:function(state){
+                state.message = 'Update!'
+            }
+        }
+    })
+}
+
+export default createStore
