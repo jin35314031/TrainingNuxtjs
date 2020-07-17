6648aca3cd1069823b8bbb333c644162f87e6ded
神馬 健介 <jin35314031@gmail.com>
2020-07-17 11:25:23 +0900

    Vuex

--- nuxt-vuex/pages/index.vue
+++ nuxt-vuex/pages/index.vue
@@ -1,10 +1,12 @@
 <template>
   <div class="container">
-    <p>{{ $store.state.message }}</p>
+    <!-- <p>{{ $store.state.message }}</p> -->
+    <p>{{ $store.state.hello.message }}</p>
     <!-- <button v-on:click="$store.commit('updateMessage')">Update</button> -->
     <!-- <button v-on:click="$store.commit('updateMessage','Commit with payload')">Update</button> -->
     <!-- <button v-on:click="$store.dispatch('updateMessageAction')">Dispatch</button> -->
-    <button v-on:click="$store.dispatch('updateMessageAction','Dispatch with payload')">Dispatch</button>
+    <!-- <button v-on:click="$store.dispatch('updateMessageAction','Dispatch with payload')">Dispatch</button> -->
+    <button v-on:click="$store.dispatch('hello/updateMessageAction','Dispatch with payload')">Dispatch</button>
   </div>
 </template>
 
