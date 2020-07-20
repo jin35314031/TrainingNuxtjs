cd645eb588f051e3a3c04537714f377086d8dde6
jin35314031 <jin35314031@gmail.com>
2020-07-20 23:33:40 +0900

    listDisplayt

--- nuxt-todo-app/pages/todos.vue
+++ nuxt-todo-app/pages/todos.vue
@@ -1,12 +1,43 @@
 <template>
     <div>
+       <!-- {{ todos }} -->
+       <ul>
+            <li v-for="todo in todos" :key="todo.id">
+                <!-- {{ todos }} -->
+                {{todo.done}} {{todo.name}} {{todo.created}}
+
+            </li>
+       </ul>
+        <div class="form">
+            <form v-on:submit.prevent="add">
+                <input v-model="name">
+                <button>Add</button>
+            </form>
+        </div>
     </div>
 </template>
 
 <script>
     export default{
+        data:function(){
+            return{
+                name:'',
+                done:false
+            }
+        },
         created: function(){
             this.$store.dispatch('todos/init')
+        },
+        methods:{
+            add() {
+                this.$store.dispatch('todos/add',this.name)
+                this.name = ''
+            }
+        },
+        computed:{
+            todos(){
+                return this.$store.state.todos.todos
+            }
         }
     }
 </script>
